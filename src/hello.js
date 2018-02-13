51f6faa46d293980324517b368c8df6dc5cc51cb
Zhenyu Huang <zhenyu@Zhenyus-MacBook-Pro.local>
2018-02-13 12:07:30 -0600

    asd

--- src/hello.js
+++
@@ -1,8 +0,0 @@
-const helloUser = (req, res) => {
-	const user = req.params.user || 'Somebody'
-	res.send('Hello ' + user + '!')
-}
-
-module.exports = (app) => {
-	app.get('/:user*?', helloUser)
-}
